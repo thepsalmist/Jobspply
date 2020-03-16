d0f091cee18f9dac6dccc3b2e8f4f3346158b19f
thepsalmist <xf.xavierfrank@gmail.com>
2020-03-16 15:07:50 +0300

    add allowed hosts

--- jobspply/settings/production.py
+++ jobspply/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "ip-address", "www.myhost.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "178.62.70.232", "www.jobsearchke.com"]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
