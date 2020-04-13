23d6c4ebe9a42f738f09dd98d57c23532a792902
thepsalmist <xf.xavierfrank@gmail.com>
2020-04-13 11:51:08 +0300

    modify templates

--- jobspply/settings/production.py
+++ jobspply/settings/production.py
@@ -1,9 +1,8 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "178.62.70.232",
-                 "www.jobsearchke.com", "jobsearchke.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "178.62.70.232", "www.jobsearchke.com", "jobsearchke.com"]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
