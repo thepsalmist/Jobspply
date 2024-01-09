18ff7c64d418fe03cbfb173f06b60af0480cf2f0
Xavier Frankline <xf.xavierfrank@gmail.com>
2024-01-09 15:20:03 +0300

    update feed

--- jobs/feeds.py
+++ jobs/feeds.py
@@ -3,15 +3,18 @@ from .models import Job
 
 
 class LatestJobsFeed(Feed):
-    title = "Latest Jobs in Kenya 2021 | Jobsearchke"
+    title = "Latest Jobs in Kenya 2024 | Jobsearchke"
     link = "/job/"
-    description = "Jobs in Kenya 2021 - JobsearchKE provides the latest job vaccancies in Kenya 2021. Signup and get updates on ngo jobs, government jobs and part time jobs in Kenya. Visit our career page and courses to enhance your career"
+    description = "Jobs in Kenya 2024 - find the current job vacancies in Kenya 2024. Latest UN jobs kenya, ngo jobs kenya, government jobs kenya and part time jobs in kenya"
 
     def items(self):
-        return Job.objects.all()[:25]
+        return Job.objects.all()[:50]
 
     def item_title(self, item):
         return item.get_job_title()
 
     def item_description(self, item):
         return item.description
+
+    def item_lastupdated(self, item):
+        return item.updated
